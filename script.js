9a580d0e4585ad26024a9a3e53721d45e5957a56
Youssef Stivan <yousefestefan95@gmail.com>
2023-11-19 10:43:53 -0600

    Homepage -> navigation bar -> work item #3 part #2 - Added a menu and cancel buttons to get in and out of the side navigation bar.

---
+++ script.js
@@ -0,0 +1,13 @@
+let menuButton = document.querySelector(".menu-button");
+let cancelButton = document.querySelector(".cancel-button");
+let navBar = document.querySelector(".navbar");
+menuButton.onclick = function () {
+  menuButton.style.opacity = "0";
+  menuButton.style.pointerEvents = "none";
+  navBar.classList.add("active");
+};
+cancelButton.onclick = function () {
+  menuButton.style.opacity = "1";
+  menuButton.style.pointerEvents = "auto";
+  navBar.classList.remove("active");
+};
